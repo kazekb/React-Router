0a05b2925b23d3616a5865a27ee60df4dd21a1d6
kazekb <kendrickbrugada@gmail.com>
2021-03-24 20:18:21 -0400

    I forgot what I changed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,13 +2,15 @@ import logo from '../images/logo.svg';
 import '../App.css';
 
 export default function Projects() {
-  return (
-    <div className = "App">
-      <header className = "App-header">
-        <img src = { logo } className = "App-logo" alt = "logo"/>
-        <p>Kaze's projects </p>
+    return ( <
+        div className = "App" >
+        <header className = "App-header">
+        <img src = { logo }
+        className = "App-logo"
+        alt = "logo" />
+        <p> Kaze 's Portfolio </p>
 
-      </header>
-    </div>
-  );
+        </header>
+        </div>
+    );
 }
